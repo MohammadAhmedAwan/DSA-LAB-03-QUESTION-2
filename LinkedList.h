9f3f9cff6d9afc017185df27c7a04d191212dbf8
MohammadAhmedAwan <156419437+MohammadAhmedAwan@users.noreply.github.com>
2024-10-01 18:21:14 +0500

    Add files via upload

---
+++ LinkedList.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Node.h"
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList();
+    void insert(int val);
+    void display();
+    ~LinkedList();
+};
---
+++ Node.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* next;
+
+    Node(int val);
+};
